9470016a7cf476c1ca7736952564f1e63f1d3e69
Office Mac <officemac@offices-macbook-pro.local>
2018-05-28 12:33:57 -0700

    Set up Codable and data system

--- Todooey/Controllers/TodoListViewController.swift
+++ Todooey/Controllers/TodoListViewController.swift
@@ -17,24 +17,9 @@ class TodoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         print(dataFilePath)
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggs"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itemArray = items
-//        }
+        loadItems()
         
     }
 
@@ -116,5 +101,18 @@ class TodoListViewController: UITableViewController {
         
         self.tableView.reloadData()
     }
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("error decoding item array, \(error)")
+            }
+        }
+    }
+    
 }
 
