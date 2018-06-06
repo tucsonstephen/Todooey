444ae260021610ac42593bb79472adf996fbe8e7
Office Mac <officemac@offices-macbook-pro.local>
2018-06-06 15:39:32 -0700

    Search Bar functionality to search list only added

--- Todooey/Controllers/TodoListViewController.swift
+++ Todooey/Controllers/TodoListViewController.swift
@@ -101,13 +101,16 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("error fetching data from context")
         }
+        
+        tableView.reloadData()
+        
     }
 
 }
@@ -119,22 +122,12 @@ extension TodoListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
         
-        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
-        request.predicate = predicate
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
-        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
-        
-        request.sortDescriptors = [sortDescriptor]
+        loadItems(with: request)
 
-        do {
-            itemArray = try context.fetch(request)
-        } catch {
-            print("error fetching data from context")
-        }
-        
-        tableView.reloadData()
-        
     }
 }
 
